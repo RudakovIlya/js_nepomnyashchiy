ff3e8507e9917de577197788ea3e0f58086913c1
ilya <rudakov.alhan@gmail.com>
2022-11-02 11:36:26 +0300

    test commit

--- 01_stack_queue/app.js
+++ 01_stack_queue/app.js
@@ -1,1 +1,32 @@
 'use strict';
+
+// FIFO, FILO
+
+// Queue - First in, First out.
+
+// push/shift
+
+const arr = [];
+
+arr.push(4)
+arr.push(7);
+arr.push(9);
+// console.log(arr);
+arr.shift();
+// console.log(arr);
+
+// Stack - First In Last Out
+// push/pop
+
+const arr2 = [];
+arr2.push(1);
+arr2.push(2);
+arr2.push(3);
+arr2.push(4);
+arr2.push(5);
+console.log(arr2);
+arr2.pop();
+arr2.pop();
+arr2.pop();
+console.log(arr2);
+console.log("1234");
